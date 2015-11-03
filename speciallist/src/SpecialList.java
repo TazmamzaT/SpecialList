0413ee9fc0602ab2c8559a21162fae9167ffb4fb
erpass <erpass@141.219.201.199>
2015-11-02 22:56:21 -0500

    DONE

--- speciallist/src/SpecialList.java
+++ speciallist/src/SpecialList.java
@@ -1,201 +1,256 @@
-import java.awt.*;
-import javax.swing.*;
-import java.util.*;
+import java.util.Vector;
 
 /**
- * A SpecialList is a singly-linked list without header/trailer nodes.
- * What is special about it is that although duplicate values are allowed, there
- * will never be two consecutive elements with the same value.
- * For example, [4,9,4] is ok, but [4,4,9] should never occur.
+ * A SpecialList is a singly - linked list without header / trailer nodes. What
+ * is special about it is that although duplicate values are allowed, there will
+ * never be two consecutive elements with the same value. For example, [4, 9, 4]
+ * is ok, but [4, 4, 9] should never occur.
  */
 @SuppressWarnings("serial")
 public class SpecialList extends AbstractSpecialList {
-	private Node top = null;
 	private int length = 0;
 
-    /**
-     * Add an value to the list at a given position.
-     * All values starting at that position are moved over one position.
-     * If the position is less than or equal to 0, the new value is added to
-     * the front of the list.
-     * If the position is greater than or equal to the number of elements in
-     * the list, the new value is added to the end of the list.
-     * Make sure the resulting list does not have two consecutive equal values.
-     *
-     * @param pos The position.
-     * @param value The value to be added.
-     */
+	/**
+	 * Add an value to the list at a given position. All values starting at that
+	 * position are moved over one position. If the position is less than or
+	 * equal to 0, the new value is added to the front of the list. If the
+	 * position is greater than or equal to the number of elements in the list,
+	 * the new value is added to the end of the list. Make sure the resulting
+	 * list does not have two consecutive equal values.
+	 *
+	 * @param pos
+	 *            The position.
+	 * @param value
+	 *            The value to be added.
+	 */
 	public void add(int pos, int value) {
-    	if (length == 0) {
-    		top = new Node(value, null);
-    		length++;
-    	} else if ( pos <= 0 && value != top.getValue() ) {
-    		top = new Node(value, top);
-    		length++;
-    	} else if ( pos >= this.length() ) { 
-    		Node current = top;
-    		while (current.getNext() != null) {
-    			current = current.getNext();
-    		}
-    		if (current.getValue() != value) {
-	    		current.setNext(new Node(value, null));
-	    		length++;
-    		}
-    	} else {
-    		Node current = top;
-    		for (int i = 0; i < pos; i++) {
-    			current = current.getNext();
-    		}
-    		if (current.getValue() != value) {
-    			Node addMe = new Node (value, current.getNext());
-        		current.setNext(addMe);
-        		length++;
-    		}
-    	}
-    	
+		if (first == null) {
+			first = new Node(value, null);
+			length++;
+		} else if (pos <= 0) {
+			if (value != first.getValue()) {
+				Node current = first;
+				first = new Node(value, current);
+				length++;
+			}
+		} else if (pos >= length()) {
+			Node current = first;
+			while (current.getNext() != null) {
+				current = current.getNext();
+			}
+			if (current.getValue() != value) {
+				current.setNext(new Node(value, null));
+				length++;
+			}
+		} else { 
+			Node current = first;
+			Node previous = null;
+			for (int i = 0; i < pos; i++) {
+				previous = current;
+				current = current.getNext();
+			}
+			if (current.getValue() != value) {
+				Node addMe = new Node(value, current);
+				previous.setNext(addMe);
+				length++;
+			}
+		}
 	} // end of add method
 
-    /**
-     * Get the current length of the list.
-     *
-     * @return The current length.
-     */
-    public int length() {
-    	return length;
+	/**
+	 * Get the current length of the list.
+	 *
+	 * @return The current length.
+	 */
+	public int length() {
+		if (first == null) {
+			return 0;
+		} else {
+			return length;
+		}
 	} // end of length method
 
-    /**
-     * Remove all values from the list.
-     */
-    public void clear() {
-    	if (length != 0 || this != null) {
-    		Node current = top;
-    		top = null;
-    		while (current != null) {
-    			Node next = current.getNext();
-    			current.setNext(null);
-    			current = next;
-    		}
-    	}
+	/**
+	 * Remove all values from the list.
+	 */
+	public void clear() {
+		if (length != 0 || this != null) {
+			Node current = first;
+			first = null;
+			while (current != null) {
+				Node next = current.getNext();
+				current.setNext(null);
+				current = next;
+			}
+		}
+		length = 0;
 	} // end of clear method
 
-    /**
-     * Get the value at the given position.
-     *
-     * @param pos The position to get the value from.
-     *            If position is less than 0, the first element is assumed.
-     *            If position is greater than or equal to the number of
-     *            elements in the list, the last element is assumed
-     *
-     * @return The value at the given position.
-     *         If there are no values in the list, 0 is returned.
-     */
-    public int valueAt(int pos) {
-    	if (length == 0) {
-    		return 0;
-    	} else if ( pos <= 0 ) {
-    		return top.getValue();
-    	} else if ( pos >= this.length() ) { 
-    		Node current = top;
-    		while (current.getNext() != null) {
-    			current = current.getNext();
-    		}
-    		return current.getValue();
-    	} else {
-    		Node current = top;
-    		for (int i = 0; i < pos - 1; i++) {
-    			current = current.getNext();
-    		}
-    		return current.getValue();
-    	}
+	/**
+	 * Get the value at the given position.
+	 *
+	 * @param pos
+	 *            The position to get the value from. If position is less than
+	 *            0, the first element is assumed. If position is greater than
+	 *            or equal to the number of elements in the list, the last
+	 *            element is assumed
+	 *
+	 * @return The value at the given position. If there are no values in the
+	 *         list, 0 is returned.
+	 */
+	public int valueAt(int pos) {
+		if (first == null) {
+			return 0;
+		} else if (pos < 0) {
+			return first.getValue();
+		} else if (pos >= length) {
+			Node current = first;
+			while (current.getNext() != null) {
+				current = current.getNext();
+			}
+			return current.getValue();
+		} else {
+			Node current = first;
+			for (int i = 0; i < pos; i++) {
+				current = current.getNext();
+			}
+			return current.getValue();
+		}
 	} // end of valueAt method
 
-    /**
-     * Find the positions of all occurrences of a given value.
-     *
-     * @param value The value to search for.
-     *
-     * @return The positions of the value in the list (the positions
-     *         should be in increasing order).
-     * 	       If the value is not in the list, return a vector with no
-     * 	       elements.
-     */
-    public Vector<Integer> positionsOf(int value) {
-    	Vector<Integer> vector = new Vector<Integer>();
-    	int count = 0;
-    	if (this != null) {
-    		Node current = top;
-    		while (current != null) {
-    			if (value == current.getValue()) {
-    				vector.add(count);
-    			}
-    		}
-    		count++;
-    	}
-	return vector;
+	/**
+	 * Find the positions of all occurrences of a given value.
+	 *
+	 * @param value
+	 *            The value to search for .
+	 *
+	 * @return The positions of the value in the list (the positions should be
+	 *         in increasing order). If the value is not in the list, return a
+	 *         vector with no elements.
+	 */
+	public Vector<Integer> positionsOf(int value) {
+		Vector<Integer> vector = new Vector<Integer>();
+		for (int i = 0; i < length; i++) {
+			if (value == valueAt(i)) {
+				vector.add(i);
+			}
+		}
+		return vector;
 	} // end of positionsOf method
 
-    /**
-     * Remove the value at the given position.
-     * Make sure the resulting list does not have two consecutive equal values.
-     *
-     * @param pos The position of the value to be omit.
-     *            If position is less than 0, the first element is assumed.
-     *            If position is greater than or equal to the number of
-     *            elements in the list, the last element is assumed
-     */
-    public void omit(int pos) {
-
+	/**
+	 * Remove the value at the given position. Make sure the resulting list does
+	 * not have two consecutive equal values.
+	 *
+	 * @param pos
+	 *            The position of the value to be omit. If position is less than
+	 *            0, the first element is assumed. If position is greater than
+	 *            or equal to the number of elements in the list, the last
+	 *            element is assumed
+	 */
+	public void omit(int pos) {
+		if (first != null) {
+			if (pos <= 0) {
+				Node oldfirst = first;
+				first = first.getNext();
+				oldfirst.setNext(null);
+				length--;
+			} else if (pos >= length() - 1) {
+				if (length > 1) {
+					Node current = first;
+					Node previous = null;
+					while (current.getNext() != null) {
+						previous = current;
+						current = current.getNext();
+					}
+					previous.setNext(null);
+					length--;
+				} else {
+					clear();
+				}
+			} else {
+				Node current = first;
+				Node previous = null;
+				for (int i = 0; i < pos; i++) {
+					previous = current;
+					current = current.getNext();
+				}
+				previous.setNext(current.getNext());
+				length--;
+				if (previous.getValue() == previous.getNext().getValue()) {
+					omit(pos);
+				}
+			} 
+		}
 	} // end of omit method
 
-    /**
-     * Shuffle the list.
-     * Split the list in half (with the extra element if the list has an odd
-     * number of elements) in the first half.
-     * Then construct a new list by starting with the first element if the
-     * first half, then appending the first element of the second list, then
-     * appending the second element of the first list, then appending the
-     * second element of the second list, etc.
-     * For example, if the list is [1,2,3,4,5,6,7], then the list after
-     * shuffling is [1,5,2,6,3,7,4].
-     * Make sure the resulting list does not have two consecutive equal values.
-     */
-    public void shuffle() { 
-    	int mod = length % 2;
-    	Node current = top;
-    	SpecialList half1 = new SpecialList();
-    	SpecialList half2 = new SpecialList();
-    	if (mod == 0) {
-    		while(current.getNext() != null) {
-    			for (int i = 0; i < length / 2; i++) {
-    				half1.add(i, this.valueAt(i));
-    			}
-    			for (int i = length / 2; i < length / 2; i++) {
-    				half2.add(i, this.valueAt(i));
-    			}
-    		}
-    	} else if (mod == 1) {
-    		while(current.getNext() != null) {
-    			for (int i = 0; i < (int)(length / 2 + 1); i++) {
-    				half1.add(i, this.valueAt(i));
-    			}
-    			for (int i = (int)(length / 2); i < (int)(length / 2); i++) {
-    				half2.add(i, this.valueAt(i));
-    			}
-    		}
-    	}
+	/**
+	 * Shuffle the list. Split the list in half (with the extra element if the
+	 * list has an odd number of elements) in the first half. Then construct a
+	 * new list by starting with the first element if the first half, then
+	 * appending the first element of the second list, then appending the second
+	 * element of the first list, then appending the second element of the
+	 * second list, etc. For example, if the list is [1, 2, 3, 4, 5, 6, 7], then
+	 * the list after shuffling is [1, 5, 2, 6, 3, 7, 4]. Make sure the
+	 * resulting list does not have two consecutive equal values.
+	 */
+	public void shuffle() {
+		if (first != null || length != 1 || length != 2) {
+			SpecialList half1 = new SpecialList();
+			SpecialList half2 = new SpecialList();
+			if (length % 2 == 0) {
+				for (int i = 0; i < length; i++) {
+					if (i < length / 2) {
+						half1.add(i, valueAt(i));
+					} else {
+						half2.add(i, valueAt(i));
+					}
+				}
+			} else {
+				for (int i = 0; i < length; i++) {
+					if (i < (length / 2) + 1) {
+						half1.add(i, valueAt(i));
+					} else {
+						half2.add(i, valueAt(i));
+					}
+				}
+			}
+			int oldlength = length;
+			clear();
+			for (int i = 0; i < oldlength; i++) {
+				if (i % 2 == 0) {
+					add(i, half1.valueAt(0));
+					half1.omit(0);
+				} else {
+					add(i, half2.valueAt(0));
+					half2.omit(0);
+				}
+			}
+		}
 	} // end of shuffle method
 
-    /**
-     * Move the largest value in the list to the end of the list.
-     * If the largest value occurs more than once, move the last occurrence of
-     * that value to the end.
-     * For example, if the list is [4,9,3,2,9,6,0], then afterwards is
-     * [4,9,3,2,6,0,9].
-     * Make sure the resulting list does not have two consecutive equal values.
-     */
-    public void move() { 
+	/**
+	 * Move the largest value in the list to the end of the list. If the largest
+	 * value occurs more than once, move the last occurrence of that value to
+	 * the end. For example, if the list is [4, 9, 3, 2, 9, 6, 0], then afterwards is
+	 * [4, 9, 3, 2, 6, 0, 9]. Make sure the resulting list does not have two
+	 * consecutive equal values.
+	 */
+	public void move() {
+		if (first != null && length != 1) {
+			int largest = Integer.MIN_VALUE;
+			Node current = first;
+			while (current.getNext() != null) {
+				if (current.getValue() > largest) {
+					largest = current.getValue();
+				}
+				current = current.getNext();
+			}
+			Vector<Integer> indexes = positionsOf(largest);
+			add(length, largest);
+			omit(indexes.lastElement());
+		}
 
 	} // end of move method
 
-    } // end of SpecialList class
+} // end of SpecialList class
--- speciallist/src/SpecialListTest.java
+++ speciallist/src/SpecialListTest.java
@@ -1,28 +1,67 @@
 import static org.junit.Assert.*;
 
+import java.awt.List;
+
 import org.junit.Test;
 
 public class SpecialListTest {
+	
 
+//	@Test
+//	public void addTest() {
+//		SpecialList test = new SpecialList();
+//		test.add(0, 0);
+//		test.add(1, 1);
+//		test.add(2, 3);
+//		test.add(1, 5);
+//		test.add(1, 5);
+//		for (int i = 0; i < test.length(); i++) {
+//			System.out.println(test.valueAt(i));
+//		}
+//		System.out.println();
+//	}
+//	
+//	@Test
+//	public void omitTest() {
+//		SpecialList test = new SpecialList();
+//		test.add(0, 0);
+//		test.add(1, 1);
+//		test.add(2, 2);
+//		test.add(3, 1);
+//		test.omit(2);
+//		for (int i = 0; i < test.length(); i++) {
+//			System.out.println(test.valueAt(i));
+//		}
+//	}
+	
+//Test
+//	public void moveTest() {
+//		int[] stuff = {12,17,50,17,46};
+//		int[] actual = {12,17,46,50};
+//		SpecialList test = new SpecialList();
+//		for (int i = 0; i < 5; i++) {
+//			test.add(i, stuff[i]);
+//		}
+//		test.move();
+//		for ()
+//	}
+	
 	@Test
-	public void addTest() {
+	public void moveTest() {
 		SpecialList test = new SpecialList();
-		test.add(0, 10);
-		test.add(1, 20);
-		System.out.println();
-		if (test.length() != 2 && test.valueAt(0) != 10 && test.valueAt(1) != 20) {
-			fail("The length was not correct");
+		test.add(0 , 0);
+		test.add(1 , 5);
+		test.add(2 , 2);
+		test.move();
+		for (int i = 0; i < test.length(); i++) {
+			//System.out.println(test.valueAt(i));
 		}
 	}
 	
 	@Test
-	public void valueAtTest() {
+	public void lengthTest() {
 		SpecialList test = new SpecialList();
-		test.add(0, 10);
-		test.add(1, 20);
-		if (test.valueAt(0) != 10) {
-			fail("The value at failed for pos 0 and value 10");
-		}
+		
 	}
 
 }
